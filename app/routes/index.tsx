7aa75a7a3f37b37b4c07c7b4214c8b57dea0465b
Austin Bourgerie <austin@bourg.me>
2025-03-28 08:53:31 -0400

    Session

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,47 +1,22 @@
-import * as fs from 'node:fs';
 import { createFileRoute, useRouter } from '@tanstack/react-router';
 import { createServerFn } from '@tanstack/react-start';
-
-const filePath = 'count.txt';
-
-async function readCount() {
-  return parseInt(
-    await fs.promises.readFile(filePath, 'utf-8').catch(() => '0'),
-  );
-}
-
-const getCount = createServerFn({
-  method: 'GET',
-}).handler(() => {
-  return readCount();
-});
-
-const updateCount = createServerFn({ method: 'POST' })
-  .validator((d: number) => d)
-  .handler(async ({ data }) => {
-    const count = await readCount();
-    await fs.promises.writeFile(filePath, `${count + data}`);
-  });
+import { useSession } from '@tanstack/react-start/server';
 
 export const Route = createFileRoute('/')({
   component: Home,
-  loader: async () => await getCount(),
+  loader: async () => {
+    const session = await useSession({
+      password: 'TODO CHANGE THIS OR FACE MY OWN WRATH',
+    });
+
+    return {
+      session,
+    };
+  },
 });
 
 function Home() {
-  const router = useRouter();
   const state = Route.useLoaderData();
 
-  return (
-    <button
-      type="button"
-      onClick={() => {
-        updateCount({ data: 1 }).then(() => {
-          router.invalidate();
-        });
-      }}
-    >
-      Add 1 to {state}?
-    </button>
-  );
+  return <pre>{JSON.stringify(state, null, 2)}</pre>;
 }
